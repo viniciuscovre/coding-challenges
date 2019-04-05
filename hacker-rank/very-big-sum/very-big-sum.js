5afe9adc6ca2d3aec6c9283b30c9f3ec6483bd3f
vinacovre <viniassis@me.com>
2019-04-05 16:39:42 -0300

    wip very big sum

---
+++ hacker-rank/very-big-sum/very-big-sum.js
@@ -0,0 +1,16 @@
+const aVeryBigSum = ar => {
+  
+}
+
+const hasValidLength = arr => {
+  const min = 1;
+  const max = 10;
+  return arr.length === size;
+}
+
+const numbersInRange = arr => {
+  const min = 0;
+  const max = 1000;
+  const foundOutOfRange = arr.find(el => el <= min || el > max);
+  return foundOutOfRange ? false : true;
+}
