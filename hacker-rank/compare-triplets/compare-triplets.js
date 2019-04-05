db621804b5467d71d63a80cd4d403e19d12518b2
vinacovre <viniassis@me.com>
2019-04-05 16:14:32 -0300

    implement main logic

--- hacker-rank/compare-triplets/compare-triplets.js
+++ hacker-rank/compare-triplets/compare-triplets.js
@@ -1,3 +1,41 @@
 const compareTriplets = (a, b) => {
-  
+  verifyInput(a);
+  verifyInput(b);
+
+  let counterA = 0;
+  let counterB = 0;
+
+  let itemB;
+
+  a.forEach((itemA, idx) => {
+    itemB = b[idx];
+    if (itemA > itemB)
+      counterA++;
+    else if (itemA < itemB)
+      counterB++;
+  })
+
+  return [counterA, counterB];
+}
+
+const verifyInput = arr => {
+  if (!hasValidLength(arr) || !hasOnlyIntegers(arr) || !numbersInRange(arr))
+    return [];
+}
+
+const hasValidLength = arr => {
+  const size = 3;
+  return arr.length === size;
+}
+
+const hasOnlyIntegers = arr => {
+  const foundNotInteger = arr.some(el => !Number.isInteger(el));
+  return foundNotInteger ? false : true;
+}
+
+const numbersInRange = arr => {
+  const min = 1;
+  const max = 1000;
+  const foundOutOfRange = arr.find(el => el < min || el > max);
+  return foundOutOfRange ? false : true;
 }
