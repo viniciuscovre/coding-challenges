011f5a5b1a3b47b9599b0fe164a66a2e449ab579
vinacovre <viniassis@me.com>
2019-08-14 00:08:42 -0300

    finish a very big sum

--- hacker-rank/very-big-sum/very-big-sum.js
+++ hacker-rank/very-big-sum/very-big-sum.js
@@ -1,16 +1,23 @@
-const aVeryBigSum = ar => {
-  
+const MAX_ARR_SIZE = 10;
+const MAX_ITEM_VAL = Math.pow(10, 10);
+
+const exampleArr = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
+
+const aVeryBigSum = arr => {
+  if (!hasValidLength(arr)) throw new Error('Invalid length');
+  if (!numbersInRange(arr)) throw new Error('Number out of range');
+
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
 const hasValidLength = arr => {
-  const min = 1;
-  const max = 10;
-  return arr.length === size;
+  const arrLen = arr.length;
+  return arrLen > 0 && arrLen <= MAX_ARR_SIZE;
 }
 
 const numbersInRange = arr => {
-  const min = 0;
-  const max = 1000;
-  const foundOutOfRange = arr.find(el => el <= min || el > max);
+  const foundOutOfRange = arr.find(el => el < 0 || el > MAX_ITEM_VAL);
   return foundOutOfRange ? false : true;
 }
+
+console.log(aVeryBigSum(exampleArr));
